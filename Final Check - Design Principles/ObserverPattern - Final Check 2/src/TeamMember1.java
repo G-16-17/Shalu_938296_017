38d0be39e2673cae4482023166285f270f219701
Shalu-Rawat <88828186+Shalu-Rawat@users.noreply.github.com>
2021-08-23 17:29:38 +0530

    Add files via upload

---
+++ Final Check - Design Principles/ObserverPattern - Final Check 2/src/TeamMember1.java
@@ -0,0 +1,11 @@
+public class TeamMember1 implements INotificationObserver{
+
+	
+	
+	@Override
+	public void OnBooking() {
+		System.out.println("Member 1 received message");
+	}
+
+
+}
---
+++ Final Check - Design Principles/ObserverPattern - Final Check 2/src/TeamMember2.java
@@ -0,0 +1,9 @@
+public class TeamMember2 implements INotificationObserver{
+	
+	
+	@Override
+	public void OnBooking() {
+		System.out.println("Member 2 received message");
+	}
+
+}
